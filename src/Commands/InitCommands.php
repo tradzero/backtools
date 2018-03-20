3f7e2db07269fea6224ba90bba48d155b99c45e5
ZHANGXIANGLIANG <326277403@qq.com>
2018-03-20 17:08:39 +0800

    [命令] 修改脚本命令为 backtools:init 和配置文件加载

--- src/Commands/InitCommands.php
+++ src/Commands/InitCommands.php
@@ -4,17 +4,18 @@ namespace TongLei\BackTools\Commands;
 
 use Illuminate\Foundation\Console\VendorPublishCommand;
 
-class PublishesCommands extends VendorPublishCommand
+class InitCommands extends VendorPublishCommand
 {
-    protected $signature = 'backtools:publishes {--force : 强制覆盖已经存在的文件}';
+    protected $signature = 'backtools:init';
 
-    protected $description = '生成后台基础 blade 模板';
+    protected $description = '生成基础 blade 模板 和 配置数据';
 
     public function handle()
     {
         $files = [
             __DIR__ . "/../public" => public_path('backtools'),
             __DIR__ . '/../resources/views' => resource_path("views/backtools"),
+            __DIR__ . '/../config' => config_path('/'),
         ];
 
         foreach ($files as $from => $to) {
