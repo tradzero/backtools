f3c2c1c6bdfa8ba199c310f6a608040745bdd880
ZHANGXIANGLIANG <326277403@qq.com>
2018-03-21 11:17:56 +0800

    [视图] 增加上传图片例子视图

---
+++ src/resources/views/templates/upload.blade.php
@@ -0,0 +1,33 @@
+@extends('backtools.master')
+
+@section('page_title')上传图片例子@endsection
+@section('title')上传图片例子@endsection
+@section('subtitle')@endsection
+
+@section('content')
+<div class="wrapper" id="app">
+    <div class="row">
+        <image-upload
+            type="s3"
+            album="/"
+            origin-image=""
+            category="test"
+            placeholder="请上传封面"
+            base-url="{{config('backtool.s3.url')}}"
+            presigned-url="{{route('backtool.s3.presigned')}}">
+        </image-upload>
+    </div>
+</div>
+@endsection
+
+@include('backtools.upload')
+@section('script')
+<script src="{{ config('backtool.cdn.js.vue') }}"></script>
+<script src="{{ config('backtool.cdn.js.axios') }}"></script>
+<script src="{{ config('backtool.cdn.js.sweetalert') }}"></script>
+<script src="{{ config('backtool.cdn.js.uuid') }}"></script>
+<script src="{{ config('backtool.cdn.js.upload-image') }}"></script>
+<script>
+    new Vue({}).$mount("#app");
+</script>
+@endsection
