bbb635c7c45c39c07d7fa4cd3f37d1fad4828120
BitMaster <326277403@qq.com>
2018-06-22 10:38:30 +0800

    [资源] [上传器] 修改 qiniu 上传器

--- src/public/js/upload-image.js
+++ src/public/js/upload-image.js
@@ -5,9 +5,11 @@ Vue.component('image-upload', {
     'album',
     'category',
     'placeholder',
+    'token',
     'baseUrl',
     'originImage',
     'presignedUrl',
+    'qiniuSignedUrl',
   ],
   data: function () {
     return {
@@ -65,20 +67,20 @@ Vue.component('image-upload', {
       });
     },
     qiniuUpload: function (file, filename) {
-      var url = axios.get(this.presignedUrl, { params: {
-        filename: filename,
-        filetype: file.type,
-        album: self.album,
-      }}).then(function (res) {
-        var options = { headers: {'Content-Type': file.type }};
-        var signedUrl =  res.data.data.signedUrl;
-        return axios.put(signedUrl, file, options);
-      }).then(function (result) {
-        self.image = filename;
-        self.$emit('success', filename);
-      }).catch(function (err) {
-        alert('上传失败');
-      });
+      var self = this;
+      axios.get(this.presignedUrl)
+        .then(function (res) {
+          var form = new FormData();
+          form.append('file', file);
+          form.append('key', filename);
+          form.append('token', res.data.data.signedUrl);
+          return axios.post(self.qiniuSignedUrl, form);
+        }).then(function () {
+          self.image = filename;
+          self.$emit('success', filename);
+        }).catch(function (err) {
+          alert('上传失败');
+        });
     }
   }
 });
